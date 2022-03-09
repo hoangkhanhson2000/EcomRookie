25398ce3f7de56613adf57b230690a49bf14ac45
hoangkhanhson2000 <hoangkhanhson2000@gmail.com>
2022-03-09 11:36:15 +0700

    update SideBar react

--- src/Rookie.Ecom.Web/ClientApp/src/components/Layout.js
+++ src/Rookie.Ecom.Web/ClientApp/src/components/Layout.js
@@ -1,12 +1,25 @@
 import React from 'react';
 import { Container } from 'reactstrap';
 import NavMenu from './NavMenu';
+import Sidebar from './Sidebar';
 
 export default props => (
   <div>
-    <NavMenu />
-    <Container>
-      {props.children}
-    </Container>
+        <NavMenu />
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-sm-1">
+                    <Sidebar />
+                </div>
+                <div class="col">
+                    <Container>
+                        {props.children}
+                    </Container>
+                </div>
+                
+            </div>
+        </div>
+        
+    
   </div>
 );
